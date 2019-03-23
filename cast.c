63e1f24a18b1ec31d8dcdf1c1b6b3874552db22c
ATruglio <andreat72001@gmail.com>
2019-03-23 12:30:56 +0000

    Correzione errori

--- cast.c
+++ cast.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
 int main(){
-double a;
-int b;
-printf("Inserire un valore con la virgola");
-scanf("%lf",&a);
-b=(int)a;
-printf("Il numero double e %lf\t",a);
-printf("Il numero intero e %d\t",b);
-return 0;
+	double a;
+	int b;
+
+	printf("Inserire un valore con la virgola");
+	scanf("%lf", &a);
+
+	b = (int)a;
+
+	printf("Il numero double e %f\t", a);
+	printf("Il numero intero e %d\t", b);
+
+	return 0;
 }
